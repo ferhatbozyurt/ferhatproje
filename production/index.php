02b6d76f4a916eca1612b79ca2ba1c48995a47cd
Ferhat Yasin Bozyurt <ferhatbozyurt@users.noreply.github.com>
2020-03-03 16:06:14 +0300

    proje pertama

---
+++ production/index.php
@@ -0,0 +1,105 @@
+<?php
+include 'header.php';
+
+?>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+           
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Admin Panel<small>Kullanıcılar</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                   
+                    <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Ad , Soyad</th>
+                          <th>Telefon</th>
+                          <th>Ülke</th>
+                          <th>Mail</th>
+                          <th>Kayıt Tarihi</th>
+                          <th>Sifre</th>
+                        </tr>
+                      </thead>
+
+
+                      <tbody>
+                        <tr>
+                          <td>Ferhat Yasin Bozyurt</td>
+                          <td>5073399552</td>
+                          <td>Türkiye</td>
+                          <td>fyb5252@gmail.com</td>
+                          <td>01/05/2019</td>
+                          <td>123456</td>
+                        </tr>
+                        <tr>
+                          <td>Cihat AKsu</td>
+                          <td>123456789</td>
+                          <td>Türkiye</td>
+                          <td>cihat@gmail.com</td>
+                          <td>02/05/2019</td>
+                          <td>112233</td>
+                        </tr>
+                        <tr>
+                          <td>Ashton Cox</td>
+                          <td>5556663322</td>
+                          <td>USA</td>
+                          <td>ashton@gmail.com</td>
+                          <td>03/05/2019</td>
+                          <td>998877</td>
+                        </tr>
+                        <tr>
+                          <td>Cedric Kelly</td>
+                          <td>555566666</td>
+                          <td>England</td>
+                          <td>Cedric@gmail.com</td>
+                          <td>04/05/2019</td>
+                          <td>663399</td>
+                        </tr>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Bitiyor -->
+
+             
+
+              
+
+              
+
+              
+             
+        <!-- /page content -->
+
+       <?php
+include 'footer.php';
+
+?>
