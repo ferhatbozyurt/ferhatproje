9e687941d5859eca0bc33f8bc9f9ae9a71f937e0
Ferhat Yasin Bozyurt <ferhatbozyurt@users.noreply.github.com>
2020-04-07 21:52:11 +0300

    rezervasyon surecinin 2. kismi tamalanmis olup arac secme ekrani yapilmistir.

---
+++ arac-secme.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+
+	<title>Transfer Arama Sonuçları - Kuşadası Transfer</title>
+
+	
+	
+
+
+
+	<!-- Kullanılanlar -->
+	<link rel='stylesheet' id='transfers-style-main-css'  href='../wp-content/themes/Transfers/css/style5152.css?ver=1.0' type='text/css' media='all' />
+	<link rel='stylesheet' id='transfers-style-color-css'  href='../wp-content/themes/Transfers/css/theme-dblue5152.css?ver=1.0' type='text/css' media='all' />
+	<script type='text/javascript' src='../wp-includes/js/jquery/jquery4a5f.js?ver=1.12.4-wp'></script>
+	<script type='text/javascript' src='../wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1'></script>
+	
+
+
+
+
+</head>  
+<body class="page-template page-template-page-advanced-search page-template-page-advanced-search-php page page-id-114 theme-Transfers woocommerce-no-js">
+
+	<main class="main " role="main">
+
+
+		<ul>
+			<li class="widget widget-sidebar">		
+				<div class="wrap">
+					<div class="row">
+						<div class="content full-width search-results-top">
+
+							<h2 class="select-departure-header">Lütfen Gidiş Transferinizi Seçin</h2>
+							<div class="results">
+								<!-- Item -->
+								<article class="result">
+									<div class="one-fourth heightfix">
+										<img src="../wp-content/uploads/2015/08/cho-thue-xe-oto-16-cho-mercedes-benz-sprinter1-870x646.jpg" alt="Mercedes Sprinter" />
+									</div>
+									<div class="one-half heightfix">
+										<h3>Private Mercedes Sprinter </h3>
+										<ul>
+											<li>
+												<span class="icon icon-themeenergy_user-3"></span>
+												<p>Maks. <strong>16 Kişilik</strong> <br />Araç</p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_date"></span>
+												<p>Gidiş Tarihi<br /><strong>Mayıs 16, 2020</strong></p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_clock"></span>
+												<p>Gidiş Saati<br /><strong>8:00 am</strong></p>
+											</li>
+										</ul>
+									</div>
+									<div class="one-fourth heightfix">
+										<div>
+											<div class="price">
+											₺ 430			</div>
+											<span class="meta">Araç Başı</span>
+											<a href="#" class="btn grey large select-avail-slot select-avail-slot-time-480 select-avail-dep-slot  select-avail-slot-private" id="select-avail-slot-22">Seç</a>
+										</div>
+									</div>
+								
+								</article>
+								<!-- //Item --><!-- Item -->
+								<article class="result">
+									<div class="one-fourth heightfix">
+										<img src="../wp-content/uploads/2015/08/1542625184000-870x348.png" alt="Mercedes Vito" />
+									</div>
+									<div class="one-half heightfix">
+										<h3>Private Mercedes Vito </h3>
+										<ul>
+											<li>
+												<span class="icon icon-themeenergy_user-3"></span>
+												<p>Maks. <strong>6 Kişilik</strong> <br />Araç</p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_date"></span>
+												<p>Gidiş Tarihi<br /><strong>Mayıs 16, 2020</strong></p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_clock"></span>
+												<p>Gidiş Saati<br /><strong>8:00 am</strong></p>
+											</li>
+										</ul>
+									</div>
+									<div class="one-fourth heightfix">
+										<div>
+											<div class="price">
+											₺ 360			</div>
+											<span class="meta">Araç Başı</span>
+											<a href="#" class="btn grey large select-avail-slot select-avail-slot-time-480 select-avail-dep-slot  select-avail-slot-private" id="select-avail-slot-14">Seç</a>
+										</div>
+									</div>
+								
+								</article>
+								<!-- //Item --><!-- Item -->
+								<article class="result">
+									<div class="one-fourth heightfix">
+										<img src="../wp-content/uploads/2015/08/passat_1.8tsi_comfortline1-870x435.png" alt="Binek" />
+									</div>
+									<div class="one-half heightfix">
+										<h3>Private Binek </h3>
+										<ul>
+											<li>
+												<span class="icon icon-themeenergy_user-3"></span>
+												<p>Maks. <strong>3 Kişilik</strong> <br />Araç</p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_date"></span>
+												<p>Gidiş Tarihi<br /><strong>Mayıs 16, 2020</strong></p>
+											</li>
+											<li>
+												<span class="icon icon-themeenergy_clock"></span>
+												<p>Gidiş Saati<br /><strong>8:00 am</strong></p>
+											</li>
+										</ul>
+									</div>
+									<div class="one-fourth heightfix">
+										<div>
+											<div class="price">
+											₺ 300			</div>
+											<span class="meta">Araç Başı</span>
+											<a href="#" class="btn grey large select-avail-slot select-avail-slot-time-480 select-avail-dep-slot  select-avail-slot-private" id="select-avail-slot-5">Seç</a>
+										</div>
+									</div>
+									
+								</article>
+								<!-- //Item -->					
+								<div class="proceed-to-booking actions"  style="display:none" >
+									<a href="odeme.php" class="btn grey huge right" id="book-transfers">Rezervasyon Yap</a>
+								</div>
+							</div>
+							<!--- // Content -->
+						</div>
+					</div>
+
+
+				</main>
+
+
+				<!-- Kullanılanlar -->
+				
+				<script type='text/javascript' src='../wp-includes/js/jquery/ui/widget.mine899.js?ver=1.11.4'></script>
+				<script type='text/javascript' src='../wp-includes/js/jquery/ui/button.mine899.js?ver=1.11.4'></script>
+				<script type='text/javascript' src='../wp-content/plugins/transfers-plugin/js/jquery-ui-timepicker-addon5152.js?ver=1.0'></script>			
+				<script type='text/javascript' src='../wp-content/plugins/transfers-plugin/js/search5152.js?ver=1.0'></script>
+				<script type='text/javascript' src='../wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min6b25.js?ver=2.1.4'></script>			
+				<script type='text/javascript' src='../wp-content/themes/Transfers/js/jquery.uniform.min5152.js?ver=1.0'></script>				
+				<script type='text/javascript' src='../wp-content/themes/Transfers/js/jquery.slicknav.min5152.js?ver=1.0'></script>
+				<script type='text/javascript' src='../wp-content/themes/Transfers/js/scripts5152.js?ver=1.0'></script>
+				
+			</body>
+
