616571710f4507a0f9dd45c86f9ab5dbfd983b26
Ferhat Yasin Bozyurt <ferhatbozyurt@users.noreply.github.com>
2020-04-28 05:57:04 +0300

    Rezervasyon 3. adimi yapildi. Burada extra ucretler icin(cocuk koltugu secme) secenekler konuldu.

---
+++ transfer3.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	
+
+
+	<title>Rezervasyon - 3</title>
+
+
+	<!-- özellikler -->
+	<link rel='stylesheet' id='transfers-font-style-css'  href='https://fonts.googleapis.com/css?family=Montserrat%3A400%2C700%7CRaleway%3A400%2C500%2C600%2C700&amp;subset=latin%2Clatin-ext&amp;ver=5.3.2' type='text/css' media='all' />
+	<link rel='stylesheet' id='transfers-style-main-css'  href='../wp-content/themes/Transfers/css/style5152.css?ver=1.0' type='text/css' media='all' />
+	<link rel='stylesheet' id='transfers-style-color-css'  href='../wp-content/themes/Transfers/css/theme-dblue5152.css?ver=1.0' type='text/css' media='all' />
+
+
+</head>  
+<body class="page-template page-template-page-booking-form page-template-page-booking-form-php page page-id-116 theme-Transfers woocommerce-no-js">
+	<!-- Preloader -->
+
+	
+	<main class="main " role="main">
+
+
+		<div class="wrap">
+			<div class="row">
+
+				<div class="full-width">
+					<div class="textongrey content">
+					</div>
+					
+					<div class="row">
+
+						<div class="full-width step1">
+							<h1>Ekstralar</h1>
+							
+						</div>
+
+						<div class="three-fourth step1">
+
+							<h3>Transfer Ekstralar</h3>
+							<table class="data responsive">
+								<tr>
+									<th>Ekstralar</th>
+									<th>Fiyat</th>
+									<th>Sayı</th>
+								</tr>
+								<tr>
+									<td>
+										<span name="dep_extra_item_title_261" id="dep_extra_item_title_261">Bebek Koltuğu</span>
+										<i>
+											9 kg ağırlığa kadar olan bebekler için											
+										</i>
+									</td>
+									<td>
+										₺ 10											
+										<input type="hidden" name="dep_extra_item_price_261" id="dep_extra_item_price_261" value="10">
+									</td>
+									<td>
+										<select class="dep_extra_item_quantity" name="dep_extra_item_quantity_261" id="dep_extra_item_quantity_261">
+											<option value="0">0</option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+										</select>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span name="dep_extra_item_title_263" id="dep_extra_item_title_263">Çocuk Koltuğu</span>
+										<i>
+											15 ila 36 kg ağırlığındaki çocuklar için											
+										</i>
+									</td>
+									<td>
+										₺ 10											
+										<input type="hidden" name="dep_extra_item_price_263" id="dep_extra_item_price_263" value="10">
+									</td>
+									<td>
+										<select class="dep_extra_item_quantity" name="dep_extra_item_quantity_263" id="dep_extra_item_quantity_263">
+											<option value="0">0</option>
+											<option value="1">1</option>
+											<option value="2">2</option>
+										</select>
+									</td>
+								</tr>
+							</table>	
+							<div class="actions">
+								<a href="arac-secme.php"  class="btn medium step1-back back">Geri Dön</a>
+								<a href="odeme.php" class="btn medium color step1-next right">Devam Et</a>
+							</div>
+						</div>
+						
+
+						<aside class="one-fourth sidebar right">
+							<!-- Widget -->
+							<div class="widget">
+								<h4>Rezervasyon Detayları</h4>
+								<div class="summary">
+									<div>
+										<h5>Gidiş Transferi</h5>
+										<dl>
+											<dt>Tarih</dt>
+											<dd class="departure_date">Nisan 28, 2020 05:09</dd>
+											<dt>Nereden</dt>
+											<dd class="departure_from">Adnan Menderes Havalimanı</dd>
+											<dt>Nereye</dt>
+											<dd class="departure_to">Kuşadası (Güzelçamlı)</dd>
+											<dt>Araç</dt>
+											<dd class="departure_transport_type">Binek</dd>
+											<dt>Kişi Sayısı</dt>
+											<dd class="departure_people">2</dd>
+											<dt>Ekstra</dt>
+											<dd class="departure_extras"></dd>
+										</dl>
+									</div>
+									<dl class="total">
+										<dt>Total</dt>
+										<dd>
+											₺ <span class="total_price_sum">300</span>										</dd>
+										</dl>
+									</div>
+								</div>
+
+							</aside>
+						</form>
+					</div>
+				</div>
+
+			</div>
+		</div>	
+	</main>
+
+</body>
+
+
+</html>
