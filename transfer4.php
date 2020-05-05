d4b445e4eaec811fa213659eda93fa707f44b7c4
Ferhat Yasin Bozyurt <ferhatbozyurt@users.noreply.github.com>
2020-05-05 10:24:18 +0300

    rezervasyon 4. adim yapilmiştir. , kişi bilgileri doldurma ve odeme bilgileri doldurma alanları yapilmistir.

---
+++ transfer4.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html PUBLIC >
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	
+	<title>Transfer 4</title>
+
+	<link href="../css/stylesheet.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+
+	<div class="main_contain">
+		<div class="contain">
+			<div class="form_top_fo">
+				<div class="top_form1">
+					<div class="top_text_fo">
+						İLETİŞİM BİLGİLERİ <span> &nbsp; </span>
+					</div>
+
+
+
+					<div class="list_form_top">
+						<div class="list_fo_left">
+
+							<ul>
+								<li class="list_1_fo"> Ad </li>
+								<li class="input_form1"><input type="text" name="cus_name" id="cus_name" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> Soyad </li>
+								<li class="input_form1"><input type="text" name="cus_surname" id="cus_surname" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> E-mail </li>
+								<li class="input_form1"><input type="text" name="email" id="email" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> Telefon </li>
+								<li class="input_form1"><input type="text" name="cus_phone" value="" /></li>
+							</ul>
+							
+
+						</div>
+						<div class="list_fo_left">
+
+
+							<ul>
+								<li class="list_1_fo"> Adres </li>
+								<li class="input_form1"><input type="text" name="cus_address" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> Şehir </li>
+								<li class="input_form1"><input type="text" name="cus_city" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> Posta Kodu </li>
+								<li class="input_form1"><input type="text" name="cus_zip_code" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> Ülke </li>
+								<li class="input_form1"><input type="text" name="cus_country" value="" /></li>
+							</ul>
+
+
+
+						</div>
+
+					</div>
+					
+				</div>
+				<div class="top_form1">
+					<div class="top_text_fo">
+						ÖDEME BİLGİLERİ <span> &nbsp; </span>
+					</div>
+
+
+
+					<div class="list_form_top">
+						<div class="list_fo_left">
+
+							<ul>
+								<li class="list_1_fo"> KART NO</li>
+								<li class="input_form1"><input type="text" name="cus_name" id="cus_name" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> KART ÜZERİNDEKİ İSİM </li>
+								<li class="input_form1"><input type="text" name="cus_surname" id="cus_surname" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> SON KULLANMA TARİHİ </li>
+								<li class="input_form1"><input type="text" name="email" id="email" value="" /></li>
+							</ul>
+							<ul>
+								<li class="list_1_fo"> CVC </li>
+								<li class="input_form1"><input type="text" name="email" id="email" value="" /></li>
+							</ul>
+
+
+
+						</div>
+					</div>
+				</div>
+
+				<div class="top_form1">
+					
+					<div class="list_form_top">
+						<div class="list_fo_left">
+
+							<div class="check_fo">
+								<input type="checkbox" /><a>Transfer şartlarını okudum,kabul ettim.</a>
+							</div>
+						</div>
+						<div class="list_fo_left">
+
+							<div class="actions">
+								<a href="index.php"  class="btn medium step1-back back">ÖDE VE TAMAMLA</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+
+
+			</form>
+
+		</div>
+	</div>
+
+
+</body>
+
+
+</html>
